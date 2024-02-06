2a5e4c506b0ee8b1f0d7cbf8edde2ad6ddee6494
Quant <nazikforall@gmail.com>
2024-02-06 18:32:58 +0200

    Show and season first step creation

--- src/main/java/com/example/springwebvideoservice/entity/Season.java
+++ src/main/java/com/example/springwebvideoservice/entity/Season.java
@@ -1,27 +1,26 @@
 package com.example.springwebvideoservice.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Data
-@Entity
-@Table(name = "shows")
 @Builder
-@Slf4j
+@Entity
+@Table
 @AllArgsConstructor
 @RequiredArgsConstructor
-
-public class Show {
+public class Season {
     @Id
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "show")
-    private List<Season> seasons;
+    @ManyToOne
+    private Show show;
+    private int numberOfEpisodes;
 
 }
